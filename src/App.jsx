aaca1a8028d44a338ecd49806daa4adce0ad3b55
BamBoos28 <119471667+BamBoos28@users.noreply.github.com>
2024-05-02 11:25:57 +0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import { Routes, Route } from 'react-router-dom'
 
 import Navbar  from './cmp/navCmp'
 
-// import Home from "./pg/homePg"
-const Home = lazy(() => import('./pg/homePg.jsx'))
-const Katalog = lazy(() => import('./pg/katalogPg.jsx'))
-const Contact = lazy(() => import('./pg/contactPg.jsx'))
+import Home from "./pg/homePg"
+import Katalog from "./pg/katalogPg"
+import Contact from "./pg/contactPg"
+// const Home = lazy(() => import('./pg/homePg.jsx'))
+// const Katalog = lazy(() => import('./pg/katalogPg.jsx'))
+// const Contact = lazy(() => import('./pg/contactPg.jsx'))
 
 export default function App() {
   return (
